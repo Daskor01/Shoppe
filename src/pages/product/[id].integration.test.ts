ac60398c0de32771a82ef4dd9ad5a64dc9a02c95
Evgenii Kurchenko <daskorwaflype01@gmail.com>
2025-12-21 11:22:36 +0300

    .

--- src/pages/product/[id].integration.test.ts
+++ src/pages/product/[id].integration.test.ts
@@ -73,7 +73,7 @@ interface ProductPageOptions {
   product?: Product | null
   isMobile?: boolean
   reviewsCount?: number
-  visibleProducts?: any[]
+  visibleProducts?: Product[]
 }
 
 // Factory for creating product page
