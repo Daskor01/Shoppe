5b33be85656436da0856497429c3c1eaccfbb17d
Evgenii Kurchenko <daskorwaflype01@gmail.com>
2026-01-05 17:48:49 +0300

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
   },
 
   app: {
-    baseURL: process.env.GITHUB_ACTIONS === 'true' ? '/shoppe-project/' : '/',
+    baseURL: process.env.GITHUB_ACTIONS === 'true' ? '/Shoppe/' : '/',
     buildAssetsDir: 'assets',
   },
 
